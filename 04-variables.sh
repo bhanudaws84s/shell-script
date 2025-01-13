dde2f6f8b7863cca8572c975ea382ee7f720de81
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-13 09:33:34 +0530

    content added

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am doing good, How about you?"
+echo "$PERSON1:: What are you doing now?"
+echo "$PERSON2::  I am a DevSecOps Engineer in Accenture $PERSON1"
+
+
