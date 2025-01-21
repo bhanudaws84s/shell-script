a00d8d654cd90440e573b9c40c52f06e746ad447
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-21 11:02:39 +0530

    added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,5 +6,5 @@ if [ $NUMBER -gt 100]
 then
     echo "Then given number is greater than 100"
 else
-    echo "The number is lessthan or equal to 100'
+    echo "The number is lessthan or equal to 100"
 fi
