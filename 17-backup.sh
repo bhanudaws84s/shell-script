e78c6b9eb8e64fe317d240aaa6f35caad7aad778
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-25 11:32:10 +0530

    added

--- 17-backup.sh
+++ 17-backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER= "/home/ec2-user/shell-logs"
+LOGS_FOLDER="/home/ec2-user/shell-logs"
 LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -20,6 +20,7 @@ USAGE(){
 }
 
 mkdir -p /home/ec2-user/shell-logs/
+
 if [ $# -lt 2 ]
 then
     USAGE
