e923423f811c1604c51634bd8a3f3fafe5ca18c7
sapbhanu2 <sapbhanu2@gmail.com>
2025-01-27 22:57:58 +0530

    added

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -2,11 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
+MSG=""
 
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "partition: $PARTITION , usage :$USAGE"
-    
+    #echo "partition: $PARTITION , usage :$USAGE"
+    if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="HIGH DISK USAGE ON PARTITIION: $PARTITION USAGE: $USAGE \n"
+    fi
 done <<< $DISK_USAGE
+
+echo "Message: $MSG"
